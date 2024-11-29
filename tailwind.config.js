964c3185dc637d4d8bfb640b24bf0acbd3eaff03
mfadhlillahrasyid <muhammadfadhlillahrasyid@gmail.com>
2024-11-29 22:47:14 +0700

    commit-6: add-assets-to-vercel.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      backgroundColor: {
+        'glass': 'rgba(255, 255, 255, 0.7)',
+      },
+      backdropBlur: {
+        'none': '0',
+        'sm': '4px',
+        'md': '10px',
+      },
     },
   },
   plugins: [
